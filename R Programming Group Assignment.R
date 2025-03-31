030cb18f158534c91a1319f0747bafa897dee735
Kok Jia Yin <jiayinkok@gmail.com>
2025-03-31 21:32:30 +0800

    Update Cleaning Data

--- R Programming Group Assignment.R
+++ R Programming Group Assignment.R
@@ -8,6 +8,7 @@ library(dplyr)
 library(janitor)    
 library(missForest) 
 library(VIM) 
+library(hms)
 
 # Set working directory
 WD <- "~/Custom Office Templates/Academic/Degree/Year 2 Sem 1/PfDA/Assignment/R Source Code" # Change to your WD
@@ -18,27 +19,43 @@ file_path <- file.path(WD, "retail_data.csv")
 retail_data <- read.csv(file_path, stringsAsFactors = FALSE)
 View(retail_data)
 
+# Remove unnecessary columns
+retail_data <- retail_data %>% select(-c(Address, Email, Phone, Name, Year, Month, Total_Amount, Zipcode))
+
+# Convert all empty strings to NA across the dataset
+retail_data[retail_data == ""] <- NA
+
+# Clean Transaction_ID
+retail_data <- retail_data |> mutate(Transaction_ID = as.character(Transaction_ID))
+# Keep distinct
+retail_data <- retail_data %>% distinct(Transaction_ID, .keep_all = TRUE)
+
+# Clean Customer_ID
+retail_data <- retail_data |> mutate(Customer_ID = as.character(Customer_ID))
+
+# Clean ZipCode
+retail_data <- retail_data |> mutate(Zipcode = as.character(Zipcode))
+
+# Clean Age
+retail_data$Age <- as.integer(retail_data$Age)
+
+# Clean Date
+retail_data$Date <- as.Date(retail_data$Date, format="%m/%d/%Y") 
+
+# Clean Time & Convert to HH:MM:SS
+retail_data$Time <- as_hms(as.POSIXct(retail_data$Time, format="%H:%M:%S")) 
+
 # Clean up and rename Total_Purchases to Purchase_Quantity for clarity
 retail_data <- retail_data |> rename(Purchase_Quantity = Total_Purchases)
 retail_data$Purchase_Quantity <- as.numeric(retail_data$Purchase_Quantity)
 
-# Clean Phone Num
-retail_data$Phone <- as.character(retail_data$Phone)
-
-# Clean Age [Take the customer age range]
-# retail_data$Age_Group <- cut(retail_data$Age, 
-#                              breaks = c(18, 25, 35, 50, 70), 
-#                              labels = c("18-25", "26-35", "36-50", "51-70"), 
-#                              right = TRUE)
-
 # Clean Amount
 retail_data$Amount <- round(as.numeric(retail_data$Amount), 2)
 
-# Truncate total amount column and replace with calculated value
-retail_data$Total_Amount <- NULL
-retail_data$Total_Amount <- retail_data$Purchase_Quantity * retail_data$Amount
+# Rename products to Products
+retail_data <- retail_data  |> rename(Products = products)
 
-
+# Categorizing
 
 
 ## Handling missing values
